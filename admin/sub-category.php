6fffda22e556bf8c48ca233ea67cbf496fc1a9fe
aramesh <arame004@odu.edu>
2020-06-03 23:05:25 -0400

    Updated Profile Page

--- admin/sub-category.php
+++ admin/sub-category.php
@@ -1,6 +1,6 @@
 <?php
 include('connect_db.php');
-$target_dir = $_SERVER["DOCUMENT_ROOT"]. "/loket_e-commerce/assets/images/collection/BigDeal_images/768-988/";
+$target_dir = $_SERVER["DOCUMENT_ROOT"]. "/loket_e-commerce/assets/images/collection/BigDeal_images/602-402/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 echo basename($_FILES["fileToUpload"]["name"]) . "<br>";
@@ -24,7 +24,7 @@ if(isset($_POST["submit"])) {
 if (file_exists($target_file)) {
     echo'<script>
     alert("Sorry, file already exists");
-    window.location = "add-product.php";
+    window.location = "add-sub_category.php";
     </script>';
   $uploadOk = 0;
 }
@@ -33,7 +33,7 @@ if (file_exists($target_file)) {
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo'<script>
     alert("Sorry, your file is too large");
-    window.location = "add-product.php";
+    window.location = "add-sub_category.php";
     </script>';
   $uploadOk = 0;
 }
@@ -43,7 +43,7 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 && $imageFileType != "gif" ) {
     echo'<script>
     alert("Sorry, only JPG, JPEG, PNG & GIF files are allowed");
-    window.location = "add-product.php";
+    window.location = "add-sub_category.php";
     </script>';
   $uploadOk = 0;
 }
@@ -52,7 +52,7 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 if ($uploadOk == 0) {
     echo'<script>
     alert("Sorry, your file was not uploaded");
-    window.location = "add-product.php";
+    window.location = "add-sub_category.php";
     </script>';
 // if everything is ok, try to upload file
 } else {
@@ -63,34 +63,29 @@ if ($uploadOk == 0) {
   }
 }
 if($uploadOk == 1){
-$product_name = $_POST['product_name'];
-$initial_cost = $_POST['intial_cost'];
-$final_cost = $_POST['final_cost'];
-$product_code = $_POST['product_code'];
 $category = $_POST['category'];
 $sub_category_name = $_POST['sub_category_name'];
-$shop_id = $_POST['shop_id'];
-$colour = $row['colour'];
-$size = $row['size'];
-$height = $row['height'];
-$weight = $row['weight'];
-$depth = $row['depth'];
-$offer_code = $row['offer_code'];
-$total_tax = 0.18;
-$product_quantity = $_POST['product_quantity'];
+// $colour = $row['colour'];
+// $size = $row['size'];
+// $height = $row['height'];
+// $weight = $row['weight'];
+// $depth = $row['depth'];
+// $offer_code = $row['offer_code'];
+// $total_tax = 0.18;
+// $product_quantity = $_POST['product_quantity'];
 $description = $_POST['description'];
-$product_image = "../assets/images/collection/BigDeal_images/768-988/".$_FILES["fileToUpload"]["name"];
+$product_image = "../assets/images/collection/BigDeal_images/602-402/".$_FILES["fileToUpload"]["name"];
 echo $product_name;
 // print_r($_POST);
-$sql = "INSERT INTO admin_products(product_id, product_name, initial_cost,final_cost, product_quantity, sub_category_name,shop_id,colour, size, height, weight, depth,offer_code, total_tax,product_image) VALUES (NULL, '$product_name','$initial_cost','$final_cost', '$product_quantity','$sub_category_name','$shop_id','$colour','$size','$height','$weight','$depth','$offer_code','$total_tax','$product_image');"; 
+$sql = "INSERT INTO admin_sub_category(id, sub_category_name, category_name,sub_category_image,description) VALUES (NULL, '$sub_category_name','$category_name','$sub_category_image','$description');"; 
 $result = $conn->query($sql);
 echo $sql;
 echo $result;
 if($result->num_rows>=0){
-    // echo'<script>
-    // alert("Product added successfully");
-    // window.location = "add-product.php";
-    // </script>';
+    echo'<script>
+    alert("Product added successfully");
+    window.location = "add-sub_category.php";
+    </script>';
 }
 }
 ?>
