8bf28a70cd1a20f8e31db10a1c0edb50be648cc4
Aditya2657 <maditya183@gmail.com>
2020-06-04 18:45:32 +0530

    track

--- html/user-tracking.php
+++ html/user-tracking.php
@@ -666,20 +666,8 @@
         <div class="row">
             <div class="col-xl-8 col-lg-6 col-md-8 offset-xl-2 offset-lg-3 offset-md-2">
                 <div class="theme-card" style="border-style:solid; border-width:5px; border-color:#d3d3d3; background-color:white;">
-                    <div class="timeline">
-                        <div class="container left">
-                            <div class="content">
-                            <h2>2017</h2>
-                            <p>Lorem ipsum..</p>
-                            </div>
-                        </div>
-                        <div class="container right">
-                            <div class="content">
-                            <h2>2016</h2>
-                            <p>Lorem ipsum..</p>
-                            </div>
-                        </div>
-                    </div> 
+                    
+                    
                 </div>
             </div>
         </div>
